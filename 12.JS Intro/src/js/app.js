e3b19de00f703c258731ff71609f8ca6051e6d50
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2023-09-12 13:00:44 +0400

    JS Intro

---
+++ 12.JS Intro/src/js/app.js
@@ -0,0 +1,121 @@
+// console.log("Hello World");
+// console.warn("Hello World");
+// console.error("Hello World");
+
+//Variables & Data Types
+// var let const
+
+// const ad = "John Doe";
+// ad = "Steve Smith";
+// console.log(ad);
+// console.log(ad);
+
+var x = 4;
+var y = "10";
+var b = true;
+var c = null;
+var d;
+// console.log(typeof(y))
+// console.log(typeof(x))
+// console.log(typeof(b))
+// console.log(typeof(c))
+// console.log(typeof(d))
+
+
+//Data Types
+//Primitive Data Types
+
+//String
+//Number
+//Boolean
+//Null
+//Undefined
+
+
+//Reference Data Types & non-primitive data types
+//Arrays
+// var nums=[1,2,3,4,5];
+// console.log(nums[3]);
+//Object Literals
+// var person={
+//     name:"Ayan",
+//     surname:"Zimina",
+//     age:33,
+//     hobbies:["music","movies","sports"],
+//     birthDate:function(){
+//         return new Date().getFullYear()-this.age;
+//     }
+// }
+// console.log(person.birthDate());
+//Functions
+//Dates
+//Classes
+
+
+// function DarkMode(){
+//     document.body.style.backgroundColor="black";
+//     document.body.style.color="white";
+//     // document.getElementById("image").style.filter="grayscale(1)";
+
+// }
+// function LightMode(){
+//     document.body.style.backgroundColor="white";
+//     document.body.style.color="black";
+//     // document.getElementById("image").style.filter="grayscale(0)";
+// }
+// alert("Hello World");
+
+
+// var time = new Date().getHours();
+
+// console.log(time);
+
+// if (time >= 6 && time < 12) {
+//     alert("Good Morning");
+// } else if (time >= 12 && time < 18) {
+//     alert("Good Afternoon");
+// } else if (time >= 18 && time < 24) {
+//     alert("Good Evening");
+// }
+
+
+//turnary operator
+var ad = "Ayan";
+ad=="Ayan" ? console.log("Hello Ayan") : console.log("You are not Ayan. Where is Ayan?");
+
+// if(ad==="Ayan"){
+//     console.log("Hello Ayan");
+// }else{
+//     console.log("You are not Ayan. Where is Ayan?");
+// }
+
+
+// if(){
+
+// }
+// else if(){
+
+// }
+// else{
+
+// }
+
+// var num=5;
+// console.log(typeof("5"))
+
+// if(num!="5"){
+//     console.log("Hello");
+// }
+// else{
+//     console.log("Bye");
+// }
+
+//Math Object
+
+// const pi=Math.PI;
+
+//  console.log(Math.round(2.6));
+console.log(Math.PI)
+console.log(Math.round(Math.random()*100));
+
+
