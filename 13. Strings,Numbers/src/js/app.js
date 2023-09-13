8e6db5358604bf4d64b156f8a0473f526cd1dbf2
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2023-09-13 11:05:32 +0400

    Strings,Numbers

---
+++ 13. Strings,Numbers/src/js/app.js
@@ -0,0 +1,114 @@
+// var text="Hello worldo! salam Worldo";
+// var text2='Hello World2!';
+
+// var text3 =new String("Hello World3!");
+
+// console.log(typeof text);
+// console.log(typeof text2);
+// console.log(typeof text3);
+
+// console.log(text3.length);
+// console.log(text.toUpperCase()); //Butun herfleri boyudur
+// console.log(text.toLowerCase()); //Butun herfleri kicildir
+// console.log(text.charAt(5)); //moterize daxilinde qeyd olunmus indexdeki simvolu verir
+// console.log(text.indexOf("o")); //moterize daxilinde qeyd olunmus simvolun indexini verir
+// console.log(text.lastIndexOf("o")); //moterize daxilinde qeyd olunmus simvolun sonuncu indexini verir
+// console.log(text.substring(3)); //moterize daxilinde qeyd olunmus indexler arasindaki simvollarin birlesmesini verir, son noqte qeyd olunmadiqda stringin sonuna qeder kesir
+// console.log(text.substring(3,5)); //moterize daxilinde qeyd olunmus indexler arasindaki simvollarin birlesmesini verir
+// console.log(text.slice(-1)); //moterize daxilinde qeyd olunmus indexden sonraki simvollarin birlesmesini verir, digerlerinden ferqi menfi deyerler qebul etmesidir
+// console.log(text.slice(0,2)); //moterize daxilinde qeyd olunmus indexler arasindaki simvollarin birlesmesini verir
+// console.log(text.split(" ")); //moterize daxilinde qeyd olunmus simvola gore stringi arraya cevirir
+// console.log(text.replace("Worldo","Javascript")); //moterize daxilinde qeyd olunmus simvolu diger simvolla evez edir
+// console.log(text.includes("Worldo")); //moterize daxilinde qeyd olunmus simvolun stringde olub olmamasini yoxlayir
+// console.log(text.startsWith("d")); //moterize daxilinde qeyd olunmus simvolun stringin baslayib baslamamasini yoxlayir
+// console.log(text.endsWith("o")); //moterize daxilinde qeyd olunmus simvolun stringin sonlanib sonlanmamasini yoxlayir
+// console.log(text.concat(" ","Salam")); //moterize daxilinde qeyd olunmus simvolu stringe elave edir
+// console.log(text.charAt(text.length-1)); //stringin sonuncu simvolunu verir
+// console.log(text.charCodeAt(0)); //moterize daxilinde qeyd olunmus indexdeki simvolun unicode deyerini verir
+// console.log(String.fromCharCode(91)); //moterize daxilinde qeyd olunmus unicode deyerdeki simvolu verir
+
+//     let text = "Please visit Microsoft! microsoft Microsoft Microsoft";
+//     let newText = text.replace(/MICROSOFT/ig, "W3Schools");
+// // let anotherText = text.replace(/Microsoft/gi, "W3Schools");
+
+// console.log(newText)
+
+// let text = "I love cats. Cats are very easy to love. Cats are very popular."
+// text = text.replaceAll("Cats","Dogs");
+// text = text.replaceAll("cats","dogs");
+// console.log(text)
+
+// var text1="Hello";
+// var text2="World";
+// console.log(text2.concat(" ",text1)); //moterize daxilinde qeyd olunmus simvolu stringe elave edir
+
+
+// let person={
+//     ad:"Ayan",
+//     soyad:"Zimina",
+//     yas:33,
+//     dogumIli:1990,
+//     cinsiyyet:"Qadin",
+//     evli:true,
+//     tehsil:"Ali",
+// }
+
+// // console.log(person.ad + " " + person.soyad + " " + person.dogumIli + "-ci ilde anadan olub");
+// // console.log(`${person.ad} ${person.soyad} ${person.dogumIli}-ci ilde anadan olub. Onun ${person.yas} yasi var`)
+
+// console.log(0.1+0.2)
+
+
+// var num=5;
+// console.log( num)
+// var num2 =num.toString();
+// console.log( num2)
+// var num2=5;
+// var num= new Number(5);
+// console.log(typeof num)
+// console.log(typeof num2)
+
+// let x = 9.656;
+
+
+// console.log(x.toExponential(1))
+// console.log(x.toExponential(4))
+// console.log(x.toExponential(6))
+
+// console.log(x.toFixed(0))
+// console.log(x.toFixed(2))
+// console.log(x.toFixed(4))
+
+//  console.log(x.toPrecision())
+//  console.log(x.toPrecision(2))
+//  console.log(x.toPrecision(4))
+
+
+// Number(true);
+// Number(false);
+// Number("10");
+// Number("  10");
+// Number("10  ");
+// Number(" 10  ");
+// Number("10.33");
+// Number("10,33");
+// Number("10 33");
+// Number("John","number");
+
+// console.log(isNaN("asdasd"))
+ 
+
+// parseInt("-10");
+// parseInt("-10.33");
+// parseInt("10");
+// parseInt("10.33");
+// parseInt("10 20 30");
+// parseInt("10 years");
+// parseInt("years 10");
+
+
+parseFloat("10");
+parseFloat("10.33");
+parseFloat("10 20 30");
+parseFloat("10 years");
+parseFloat("years 10");
